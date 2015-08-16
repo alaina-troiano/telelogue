ad2532b6097d585535eb7b26302678753ecb06cc
Alaina Rowe <alaina.troiano@gmail.com>
2015-08-16 08:38:33 -0500

    login view

--- telelogue/settings.py
+++ telelogue/settings.py
@@ -1,4 +1,5 @@
 # Django settings for telelogue project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -114,6 +115,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates/'),
 )
 
 INSTALLED_APPS = (
@@ -167,3 +169,5 @@ LOGGING = {
         },
     }
 }
+
+LOGIN_REDIRECT_URL = '/chat/'
