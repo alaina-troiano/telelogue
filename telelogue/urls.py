5d7c0e1e0b4fc274d84fea8f742b086a810b2319
Alaina Rowe <alaina.troiano@gmail.com>
2015-08-16 08:53:29 -0500

    logout

--- telelogue/urls.py
+++ telelogue/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.views import logout_then_login
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -10,6 +11,7 @@ urlpatterns = patterns(
     # url(r'^telelogue/', include('telelogue.foo.urls')),
 
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', logout_then_login, name='logout_then_login'),
 
     url(r'^chat/', include('chat.urls')),
     url(r'^transit/', include('transit.urls')),
